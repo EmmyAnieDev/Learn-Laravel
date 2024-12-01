b65d658418309e7173f5019f064b122a16b15121
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-01 21:29:21 +0100

    Basic Controller

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
 
 
 
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [HomeController::class, 'showAboutPage']);
 
 // Fallback Routes. It should be placed last in your routes/web.php or routes/api.php file 
 Route::fallback(function () {
