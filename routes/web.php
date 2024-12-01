215609f1392d9ff9b1199401547ac28ccb54c72d
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-01 18:27:59 +0100

    Extending blade (reusable files)

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
 
 // Fallback Routes. It should be placed last in your routes/web.php or routes/api.php file 
 Route::fallback(function () {
     return "Oops we couldn't find the page!";
 });
-
-
