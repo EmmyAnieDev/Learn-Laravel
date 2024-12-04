40e8227aa4d3d1fe11abc3371cff81d64ba23e59
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-04 23:22:05 +0100

    Eloquent ORM Restore Deleted data or Permanently delete data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,16 +11,20 @@ class ProductController extends Controller
 
     function index() {
 
-        Product::find(54)->delete(); 
+        // Product::find(54)->delete(); 
 
         // Retrieve all products, including those that have been soft deleted (trashed)
-        $product = Product::withTrashed()->get();
+        // $product = Product::withTrashed()->get();
 
-        $product = Product::withTrashed()->find(43);    // Retrieve just the trashed data.
+        // $product = Product::withTrashed()->find(43);    // Retrieve just the trashed data.
 
-        $product = Product::onlyTrashed()->get();   // Retreive all trashed data
+        // $product = Product::onlyTrashed()->get();   // Retreive all trashed data
 
-        dd($product);
+        // Product::withTrashed()->find(54)->restore();   // Restore a deleted data back to the table.
+
+        Product::withTrashed()->find(37)->forceDelete();  // Permanently delete the data.
+
+        // dd($product);
         
     }
     
