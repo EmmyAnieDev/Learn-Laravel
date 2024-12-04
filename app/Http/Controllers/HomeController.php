3e3c3e64e8b3de4f558354719117579ae8fd5d6d
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-04 08:54:42 +0100

    Update data in our DB using Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,11 +12,12 @@ class HomeController extends Controller
 
         // Query Builder works directly with Table, so we have to mention the table we want to query.
 
-        $users = DB::table('users')->get();    // get all users
-        $users = DB::table('users')->where('id', 6)->first();   // get user by id. with first method signifying first record found
-        $users = DB::table('users')->where('id', '>', 2)->get();   // get all user where id is greater than 2
-
-        return $users; // By default, Laravel automatically converts arrays and collections to JSON when returning them as a response.
+        DB::table('users')->where('id', 6)->update(
+            [
+                'name' => 'Janet Davis',
+                'email' => 'janet.davis@gmail.com'
+            ]
+        );
 
         return view('welcome');
     }
