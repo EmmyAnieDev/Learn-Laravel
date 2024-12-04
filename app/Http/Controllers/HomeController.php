1aa5316baed5a105db11d973fc523a5220f9e66f
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-04 08:46:45 +0100

    Getting data from DB using Query Builder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,45 +11,12 @@ class HomeController extends Controller
     function index() {
 
         // Query Builder works directly with Table, so we have to mention the table we want to query.
-        DB::table('users')->insert(
-            [
-                'name' => 'John Doe',
-                'email' => 'john@gmail.com',
-                'password' => '123456'
-            ]
-        );   // This method automatically run when our welcome page is visited.
 
-        // To insert Multiple Data at once, we wrap them inside another array which will makes it a multidimensional array
-        DB::table('users')->insert(
-            [
-                [
-                    'name' => 'Alice Smith',
-                    'email' => 'alice.smith@gmail.com',
-                    'password' => 'password123'
-                ],
-                [
-                    'name' => 'Bob Johnson',
-                    'email' => 'bob.johnson@gmail.com',
-                    'password' => 'securepass456'
-                ],
-                [
-                    'name' => 'Cathy Brown',
-                    'email' => 'cathy.brown@gmail.com',
-                    'password' => 'mypassword789'
-                ],
-                [
-                    'name' => 'David Wilson',
-                    'email' => 'david.wilson@gmail.com',
-                    'password' => 'letmein101'
-                ],
-                [
-                    'name' => 'Emily Davis',
-                    'email' => 'emily.davis@gmail.com',
-                    'password' => 'passw0rd111'
-                ]
-            ]
-            
-        ); 
+        $users = DB::table('users')->get();    // get all users
+        $users = DB::table('users')->where('id', 6)->first();   // get user by id. with first method signifying first record found
+        $users = DB::table('users')->where('id', '>', 2)->get();   // get all user where id is greater than 2
+
+        return $users; // By default, Laravel automatically converts arrays and collections to JSON when returning them as a response.
 
         return view('welcome');
     }
