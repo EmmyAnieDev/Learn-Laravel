48a7611c50961527e2cc9006d80a14eb8f191e03
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-06 05:19:29 +0100

    Form and Validation: Custom Validation Message

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,10 +13,22 @@ class ContactController extends Controller
 
     function contactSubmit(Request $request)  // Using Dependency Injection to access the data in the request
     {
-        $request->validate([
-            'name' => 'required|max:20|min:2',
-            'email' => 'required|email',
-        ]);
+        $request->validate(
+            [
+                // 'name' => 'required|max:20|min:2',
+                // 'email' => 'required|email',
+
+                'name' => ['required', 'max:20', 'min:2'],  // using the array pattern
+                'email' => ['required', 'email'],
+            ], 
+            [
+                'name.required' => 'The name field is mandatory.',
+                'name.max' => 'The name must not exceed 20 characters.',
+                'name.min' => 'The name must be at least 2 characters long.',
+                'email.required' => 'The email field is required.',
+                'email.email' => 'Please provide a valid email address.',
+            ]
+        );        
 
         dd($request->all());
     }
