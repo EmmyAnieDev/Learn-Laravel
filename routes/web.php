4d6134e407fa4b307c68aad73a02e13e859266c9
EmmyAnieDev <Emaduilzjr1@gmail.com>
2024-12-06 04:09:19 +0100

    Form and Validation: Form Markup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SingleActionController;
@@ -12,17 +13,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/about', [HomeController::class, 'showAboutPage']);
-
-Route::get('/single-action', SingleActionController::class);
-
-Route::resource('blog', BlogController::class);
-
-Route::get('/products', [ProductController::class, 'index']);
-
-Route::get('/users', [UserController::class, 'index']);
-
-// Fallback Routes. It should be placed last in your routes/web.php or routes/api.php file 
-Route::fallback(function () {
-    return "Oops we couldn't find the page!";
-});
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
